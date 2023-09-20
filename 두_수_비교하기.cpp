9ec798fde1c33432d1622ebb20134f8729d1da70
HweDongJung <asky1193@gmail.com>
2023-09-20 23:42:15 +0900

    두 수 비교하기 solved

---
+++ 두_수_비교하기.cpp
@@ -0,0 +1,15 @@
+//https://www.acmicpc.net/problem/1330
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if(a > b) cout <<">";
+    else if(a < b) cout << "<";
+    else  cout <<"==";
+    return 0;
+}
