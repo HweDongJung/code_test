2cd1340a3ce3184c2f3ca5dbcd919b7cababaa85
HweDongJung <asky1193@gmail.com>
2023-10-01 18:19:25 +0900

    solving 수들의합

---
+++ 수들의합.cpp
@@ -0,0 +1,21 @@
+//https://www.acmicpc.net/problem/2293
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin << n << k;
+    cin.ignore();
+    vector<int> list;
+    for(int i=0; i < n; i++){
+    int f;
+    cin << f;
+    list.pushback(f);
+    cin.ignore();
+    }
+
+
+    return 0;
+}
