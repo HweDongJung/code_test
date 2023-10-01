cae457518695ba216880900213215aa86bfbed2a
HweDongJung <asky1193@gmail.com>
2023-10-01 19:23:34 +0900

    일곱난쟁이 solved

---
+++ 일곱난쟁이.cpp
@@ -0,0 +1,23 @@
+//https://www.acmicpc.net/problem/2309
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    vector<int> list;
+
+    for(int i=0; i < 9; i++){
+    int f;
+    cin >> f;
+
+    cin.ignore();
+    list.push_back(f);
+    
+    }
+    
+    
+
+    return 0;
+}
