463b23910cb36e769437ace96dd7a8b96ba2865f
HweDongJung <asky1193@gmail.com>
2023-10-15 15:17:28 +0900

    18018 solved

---
+++ 18108.cpp
@@ -0,0 +1,14 @@
+//https://www.acmicpc.net/problem/18108
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cin.ignore();
+    cout << n-543;
+
+
+    return 0;
+}
