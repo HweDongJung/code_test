29da8d5133b59fa1db48472a1bf43b7390e66071
HweDongJung <asky1193@gmail.com>
2023-11-05 19:52:27 +0900

    10718 solved

---
+++ 10718.cpp
@@ -0,0 +1,13 @@
+//https://www.acmicpc.net/problem/10718
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    cout << "강한친구 대한육군\n강한친구 대한육군";
+    
+
+    return 0;
+}
