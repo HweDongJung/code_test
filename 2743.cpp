d70741a5aa03847754daf546a47453b94070eee5
HweDongJung <asky1193@gmail.com>
2023-11-21 15:49:59 +0900

    2743 solved

---
+++ 2743.cpp
@@ -0,0 +1,14 @@
+//https://www.acmicpc.net/problem/2743
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+    cin.ignore();
+
+    cout << a.length();
+
+    return 0;
+}
